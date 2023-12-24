987be549f95662739f4acff6e5e816b6add092f8
VCR01 <126965254+VCR01@users.noreply.github.com>
2023-12-24 19:01:37 +0000

    Initial Commit

---
+++ Assets/Scripts/PhysicsRig.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsRig : MonoBehaviour
+{
+    public Transform playerHead;
+    public Transform leftController;
+    public Transform rightController;
+    public CapsuleCollider bodyCollider;
+
+    public float bodyHeightMin = 0.5f;
+    public float bodyHeightMax = 2;
+
+    public ConfigurableJoint leftHandJoint;
+    public ConfigurableJoint rightHandJoint;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bodyCollider.height = Mathf.Clamp(playerHead.localPosition.y, bodyHeightMin, bodyHeightMax);
+        bodyCollider.center = new Vector3(playerHead.localPosition.x, bodyCollider.height / 2, playerHead.localPosition.z);
+
+        leftHandJoint.targetPosition = leftController.localPosition;
+        leftHandJoint.targetRotation = leftController.localRotation;
+
+        rightHandJoint.targetPosition = rightController.localPosition;
+        rightHandJoint.targetRotation = rightController.localRotation;
+    }
+}
