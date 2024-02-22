29e4fe23248840c4d0e1756fd7714999854d7caa
VCR01 <126965254+VCR01@users.noreply.github.com>
2024-02-22 18:41:37 +0000

    Fixed External Camera Bug
    Fixed a bug with the external camera where it would bug out and lag when turned on

--- Assets/Scripts/Player/AvatarMapping.cs
+++ Assets/Scripts/Player/AvatarMapping.cs
@@ -6,14 +6,26 @@ public class AvatarMapping : MonoBehaviour
 {
     public Transform[] bones;
     public Transform[] targets;
+    public Vector3[] targetsOffsetRot;
+    public Vector3[] targetsOffsetPos;
+
+    public Transform[] twists;
+    public Transform[] twistTargets;
+    public Vector3[] twistsOffsetRot;
+    public Vector3[] twistsOffsetPos;
 
     // Update is called once per frame
     void LateUpdate()
     {
         for(int i = 0; i < bones.Length; i++)
         {
-            bones[i].position = targets[i].position;
-            bones[i].rotation = targets[i].rotation;
+            bones[i].position = targets[i].position + targetsOffsetPos[i];
+            bones[i].rotation = targets[i].rotation * Quaternion.Euler(targetsOffsetRot[i]);
+        }
+        for (int i = 0; i < twists.Length; i++)
+        {
+            twists[i].position = twistTargets[i].position + twistsOffsetPos[i];
+            twists[i].rotation = twistTargets[i].rotation * Quaternion.Euler(targetsOffsetPos[i]);
         }
     }
 }
